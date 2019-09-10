698a5b152125cb0fa71e25e504b0cdff04e79513
Peri <peri@srdi.org>
2019-09-10 01:27:26 +0100

    Reverted vfs_csrb* back to posix clone from fatfs.

--- ports/CSRB/vfs_csrb.h
+++ ports/CSRB/vfs_csrb.h
@@ -3,37 +3,11 @@
 
 #include "py/lexer.h"
 #include "py/obj.h"
-#include "extmod/vfs.h"
 
-// these are the values for fs_user_mount_t.flags
-#define FSUSER_NATIVE       (0x0001) // readblocks[2]/writeblocks[2] contain native func
-#define FSUSER_FREE_OBJ     (0x0002) // fs_user_mount_t obj should be freed on umount
-#define FSUSER_HAVE_IOCTL   (0x0004) // new protocol with ioctl
-#define FSUSER_NO_FILESYSTEM (0x0008) // the block device has no filesystem on it
-
-typedef struct _fs_user_mount_t {
-#if 0
-    mp_obj_base_t base;
-    uint16_t flags;
-    mp_obj_t readblocks[4];
-    mp_obj_t writeblocks[4];
-    // new protocol uses just ioctl, old uses sync (optional) and count
-    union {
-        mp_obj_t ioctl[4];
-        struct {
-            mp_obj_t sync[2];
-            mp_obj_t count[2];
-        } old;
-    } u;
-    CSRBFS csrbfs;
-#endif
-} fs_user_mount_t;
-
-extern const byte fresult_to_errno_table[20];
-extern const mp_obj_type_t mp_csrb_vfs_type;
+extern const mp_obj_type_t mp_type_vfs_csrb;
 extern const mp_obj_type_t mp_type_vfs_csrb_fileio;
 extern const mp_obj_type_t mp_type_vfs_csrb_textio;
 
-MP_DECLARE_CONST_FUN_OBJ_3(csrb_vfs_open_obj);
+mp_obj_t mp_vfs_csrb_file_open(const mp_obj_type_t *type, mp_obj_t file_in, mp_obj_t mode_in);
 
 #endif // MICROPY_INCLUDED_EXTMOD_VFS_CSRB_H
